717701fa30f220faa2b6e3d029324cdec4fba032
Mukeshvar A <65329903+MUKESHVAR@users.noreply.github.com>
2023-05-17 16:30:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,5 @@ main = Flask(__name__)
 def index():
     return "Hello world"
 
-@main.route('/predict',methods=['POST'])
-def predict():
-    cgpa = request.form.get('cgpa')
-    iq = request.form.get('iq')
-    profile_score = request.form.get('profile_score')
-
-    input_query = np.array([[cgpa,iq,profile_score]])
-
-    result = {'cgpa':cgpa,'iq':iq,'profile_score':profile_score}
-
-    return jsonify(result)
-
 if __name__ == '__main__':
     main.run(debug=True)
